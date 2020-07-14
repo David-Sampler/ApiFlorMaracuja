111e861d0c05bf790e7af2816b8858676fd427be
sampler <davidsampler.engenharia@gmail.com>
2020-07-14 12:00:22 -0300

    ajuste

--- connectionDB/conexao.js
+++ connectionDB/conexao.js
@@ -1,5 +1,5 @@
 //conexao
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DBMONGO,{useUnifiedTopology:true,useNewUrlParser:true,useFindAndModify:false,useCreateIndex:true});
+mongoose.connect('mongodb+srv://sampler:12345@cluster0-lqjhc.mongodb.net/flor?retryWrites=true&w=majority',{useUnifiedTopology:true,useNewUrlParser:true,useFindAndModify:false,useCreateIndex:true});
 module.exports = mongoose
--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const cors = require('cors')
 }*/
 
  //SERVER HEADERS
- /*app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+ app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
   if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP 
       res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
   else //Se a requisição já é HTTPS 
       next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
-});*/
+});
 
 
  app.use((req, res, next) => {
