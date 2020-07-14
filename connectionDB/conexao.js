dd0dcaf6b3b3a6515b0852c70f53b95383328c71
sampler <davidsampler.engenharia@gmail.com>
2020-07-14 11:52:50 -0300

    ajuste 1

--- connectionDB/conexao.js
+++ connectionDB/conexao.js
@@ -1,4 +1,5 @@
 //conexao
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DBMONGO,{useUnifiedTopology:true,useNewUrlParser:true,useFindAndModify:false,useCreateIndex:true});
 module.exports = mongoose
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const restify  = require("restify")
 const app = restify.createServer({version:1.0,name:"flor"})
